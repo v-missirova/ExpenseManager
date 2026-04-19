0eb358c788e487b2cc53819277f3321448a13a2d
Вязанка <@DESKTOP-2F9HTPB>
2026-04-19 10:01:14 +0300

    lab 4 now can add transactions and nothing dies

--- ExpenseManager/ViewModels/WalletDetailsViewModel.cs
+++ ExpenseManager/ViewModels/WalletDetailsViewModel.cs
@@ -1,8 +1,9 @@
-using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using ExpenseManager.Services;
+using ExpenseManager.Common.Enums;
 using ExpenseManager.DBModels;
+using ExpenseManager.Services;
+using System.Collections.ObjectModel;
 
 namespace MauiApp1.ViewModels
 {
@@ -26,7 +27,69 @@ namespace MauiApp1.ViewModels
         public partial string WalletCurrency { get; set; }
         [ObservableProperty]
         public partial TransactionDBModel? SelectedTransaction { get; set; }
+        [ObservableProperty]
+        public partial bool IsAddFormVisible { get; set; }
 
+        [ObservableProperty]
+        public partial decimal NewTransactionAmount { get; set; }
+
+        [ObservableProperty]
+        public partial string NewTransactionDescription { get; set; }
+
+        [ObservableProperty]
+        public partial Category NewTransactionCategory { get; set; }
+
+        public List<Category> Categories { get; } = Enum.GetValues(typeof(Category)).Cast<Category>().ToList();
+
+        [RelayCommand]
+        private void ToggleAddForm() => IsAddFormVisible = !IsAddFormVisible;
+
+        [RelayCommand]
+        private async Task AddTransactionAsync()
+        {
+            if (NewTransactionAmount == 0)
+            {
+                await Shell.Current.DisplayAlertAsync("Error", "Enter sum (expenses should be negative)", "ok..");
+                return;
+            }
+
+            IsBusy = true;
+
+            try
+            {
+                var amountToSave = NewTransactionAmount;
+                var categoryToSave = NewTransactionCategory;
+                var descToSave = NewTransactionDescription;
+                var dateToSave = DateTime.Now;
+
+                await _transactionService.AddTransactionAsync(_currentWalletId, amountToSave, 
+                    categoryToSave, descToSave);
+
+                WalletBalance += amountToSave;
+
+                Transactions.Insert(0, new TransactionDBModel
+                {
+                    Id = Guid.NewGuid(),
+                    WalletId = _currentWalletId,
+                    Amount = amountToSave,
+                    Category = categoryToSave,
+                    Description = descToSave,
+                    DateTimeOfTransaction = dateToSave
+                });
+
+                NewTransactionAmount = 0;
+                NewTransactionDescription = string.Empty;
+                IsAddFormVisible = false;
+            }
+            catch (Exception ex)
+            {
+                await Shell.Current.DisplayAlertAsync("Помилка бази даних", ex.Message, "OK");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
         partial void OnSelectedTransactionChanged(TransactionDBModel? value)
         {
             if (value != null)
